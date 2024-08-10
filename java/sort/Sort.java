961fef2f588871ea89d04b98bed2014075828bb8
dhruvksuri <dhruvksuri@gmail.com>
2024-08-10 15:28:29 +0530

    Update Sort.java

--- java/sort/Sort.java
+++ java/sort/Sort.java
@@ -27,6 +27,40 @@ public class Sort {
         qSort(a, i, high);
     }
 
+        // Best: O(nlog(n)) time | O(nlog(n)) space
+		// Average: O(nlog(n)) time | O(nlog(n)) space
+		// Worst: O(nlog(n)) time | O(nlog(n)) space
+		public int[] mergeSort(int[] array) {
+			if (array.length <= 1) {
+				return array;
+			}
+			int middleIdx = array.length / 2;
+			int[] leftHalf = Arrays.copyOfRange(array, 0, middleIdx);
+			int[] rightHalf = Arrays.copyOfRange(array, middleIdx, array.length);
+			return mergeSortedArrays(mergeSort(leftHalf), mergeSort(rightHalf));
+		}
+
+		public int[] mergeSortedArrays(int[] leftHalf, int[] rightHalf) {
+			int[] sortedArray = new int[leftHalf.length + rightHalf.length];
+			int k = 0;
+			int i = 0;
+			int j = 0;
+			while (i < leftHalf.length && j < rightHalf.length) {
+				if (leftHalf[i] <= rightHalf[j]) {
+					sortedArray[k++] = leftHalf[i++];
+				} else {
+					sortedArray[k++] = rightHalf[j++];
+				}
+			}
+			while (i < leftHalf.length) {
+				sortedArray[k++] = leftHalf[i++];
+			}
+			while (j < rightHalf.length) {
+				sortedArray[k++] = rightHalf[j++];
+			}
+			return sortedArray;
+		}
+    
     public static void mergeSort(int[] a, int low, int high) {
         if (high - low < 2)
             return;
