5b889ace98cd936aaeb68e0f4cdfebc4196d06f3
dhruvksuri <dhruvksuri@gmail.com>
2024-08-11 12:11:37 +0530

    Update GraphColoringGreedy2.java

--- java/graphs/GraphColoringGreedy2.java
+++ java/graphs/GraphColoringGreedy2.java
@@ -2,6 +2,15 @@ package Graphs;
 import java.util.*;
 import java.util.stream.Stream;
 
+/**
+Graph colouring is the task of assigning colours to the vertices of a graph so that:
+1. pairs of adjacent vertices are assigned different colours, and
+2. the number of different colours used across the graph is minimal.
+
+At each step, the calculation chooses the vertex with the most no of in-degrees (i.e., the number of as-of-now colored adjoining vertices) 
+and allocates it to a color that's not as of now utilized by its neighbors. 
+This approach leads to optimized coloring, maximizing color differing qualities, and minimizing color utilization.
+*/
 public class GraphColoringGreedy2 {
 
 	public static int[] color(List<Integer>[] graph) {
@@ -10,11 +19,15 @@ public class GraphColoringGreedy2 {
 		int[] colors = new int[n];
 		Arrays.fill(colors, -1);
 
+		// Non SCC components would start re-using the same color
 		for (int i = 0; i < n; i++) {
 			int best_cnt = -1;
 			int bestu = -1;
 			for (int u = 0; u < n; u++) {
+				// If u is not colored
 				if (colors[u] == -1) {
+					// No of in-degrees is bitCount
+					// 000111 - 3 bit count means 2 in-coming edges so give color 3
 					int cnt = Integer.bitCount(used[u]);
 					if (best_cnt < cnt) {
 						best_cnt = cnt;
@@ -22,10 +35,11 @@ public class GraphColoringGreedy2 {
 					}
 				}
 			}
+			// ~used[bestu] (000111) -> 111000 -> 3 
 			int c = Integer.numberOfTrailingZeros(~used[bestu]);
-			colors[bestu] = c;
+			colors[bestu] = c; // Assign 3
 			for (int v : graph[bestu]) {
-				used[v] |= 1 << c;
+				used[v] |= 1 << c;  // 001000
 			}
 		}
 		return colors;
