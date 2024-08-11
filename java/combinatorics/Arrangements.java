cae4a06b94aadf107da44af83cc992520f687c5c
dhruvksuri <dhruvksuri@gmail.com>
2024-08-11 22:33:14 +0530

    Update Arrangements.java

--- java/combinatorics/Arrangements.java
+++ java/combinatorics/Arrangements.java
@@ -77,6 +77,26 @@ public class Arrangements {
         return res;
     }
 
+    /**
+        [0, 1, 2]
+        [0, 1, 3]
+        [0, 2, 0]
+        [0, 2, 1]
+        [0, 2, 2]
+        [0, 2, 3]
+        [0, 3, 0]
+        [0, 3, 1]
+        [0, 3, 2]
+        [0, 3, 3]
+        [1, 0, 0]
+        [1, 0, 1]
+        [1, 0, 2]
+        [1, 0, 3]
+        [1, 1, 0]
+        [1, 1, 1]
+        [1, 1, 2]
+        [1, 1, 3]
+    */
     public static boolean nextArrangementWithRepeats(int[] a, int n) {
         for (int i = a.length - 1; i >= 0; i--) {
             if (a[i] < n - 1) {
