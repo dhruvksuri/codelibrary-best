0677cec063a9629b3d57c4295f98a7655bd5e989
dhruvksuri <dhruvksuri@gmail.com>
2024-08-11 23:27:15 +0530

    Update BracketSequences.java

--- java/combinatorics/BracketSequences.java
+++ java/combinatorics/BracketSequences.java
@@ -3,6 +3,28 @@ package combinatorics;
 import java.util.Arrays;
 
 public class BracketSequences {
+    /**
+        bool next_balanced_sequence(string & s) {
+            int n = s.size();
+            int depth = 0;
+            for (int i = n - 1; i >= 0; i--) {
+                if (s[i] == '(')
+                    depth--;
+                else
+                    depth++;
+        
+                if (s[i] == '(' && depth > 0) {
+                    depth--;
+                    int open = (n - i - 1 - depth) / 2;
+                    int close = n - i - 1 - open;
+                    string next = s.substr(0, i) + ')' + string(open, '(') + string(close, ')');
+                    s.swap(next);
+                    return true;
+                }
+            }
+            return false;
+        }
+    */
     public static boolean nextBracketSequence(char[] s) {
         int n = s.length;
         for (int i = n - 1, balance = 0; i >= 0; i--) {
