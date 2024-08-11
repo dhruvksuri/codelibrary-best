8dbb9df3496e407552f69500109578b855b0b8ec
dhruvksuri <dhruvksuri@gmail.com>
2024-08-11 14:19:28 +0530

    Update BinaryExponentiation.java

--- java/misc/BinaryExponentiation.java
+++ java/misc/BinaryExponentiation.java
@@ -5,6 +5,7 @@ public class BinaryExponentiation {
     public static int pow(int x, int n, int mod) {
         int res = 1;
         for (long p = x; n > 0; n >>= 1, p = (p * p) % mod) {
+            // If number is odd then only
             if ((n & 1) != 0) {
                 res = (int) (res * p % mod);
             }
